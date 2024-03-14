dc26cd86809ef66db5fecb88554b0faae667fea6
jamiu-adeleye <oluwaloseyifunmiadeleye1@gmail.com>
2024-03-14 12:13:03 +0100

    feat: add editor types and provider starter for website-builder feature

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,13 @@
+import EditorProvider from '@/providers/editor/editor-provider'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className="fixed top-0 bottom-0 left-0 right-0 z-[20] bg-background overflow-hidden">
+      <EditorProvider>
+        page
+      </EditorProvider>
+    </div>
   )
 }
 
